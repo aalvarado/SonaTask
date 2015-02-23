8eb61fcda93692e28aa8b82372adf45d06e5aa55
aalvarado <adan.alvarado7@gmail.com>
2015-02-22 16:34:49 -0800

    Set default version

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  # TODO: how to specify header value based on env?
-  api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.sonatask.dev; version=1"}) do
-
+  # TODO: how to specify header (vnd.sonatask.dev) value based on env?
+  api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.sonatask.dev; version=1"}, default: true) do
   end
 end
