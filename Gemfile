e83a8f170bb7f8a5a389a75fe7f994ebbe528aae
aalvarado <adan.alvarado7@gmail.com>
2015-02-22 19:20:14 -0800

    Add spec helpers

--- Gemfile
+++ Gemfile
@@ -24,4 +24,5 @@ end
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'spec_helper'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryGirl.lint
+  end
+end
