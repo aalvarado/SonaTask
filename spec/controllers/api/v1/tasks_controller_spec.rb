b377316d0e2774a611fcba93fad7ab41ae6e0523
aalvarado <adan.alvarado7@gmail.com>
2015-02-23 19:01:07 -0800

    Tasks controller examples

---
+++ spec/controllers/api/v1/tasks_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe Api::V1::TasksController do
+  let( :user ) { create :user }
+  let( :task ) { create :task, user: user }
+  let( :task_attr ) { attributes_for :task }
+
+  context 'logged in user' do
+    before do
+      login_user user
+    end
+
+    describe 'show' do
+      before do
+        get :show, id: task.id
+      end
+
+      it{ expect( response ).to be_success }
+
+      it 'returns the right task' do
+        expect( response_body_object.id ).to eq task.id
+      end
+    end
+
+    describe 'create' do
+
+      before do
+        post :create, task: task_attr
+      end
+
+      it{ expect( response ).to be_success }
+    end
+
+    describe 'update' do
+      before do
+        put :update, id: task.id, task: task_attr
+      end
+
+      it{ expect( response ).to be_success }
+
+      it 'returns the task with updated attributes' do
+        get :show, id: task.id
+
+        task_attr.each do |k,v|
+          expect( response_body_object.send k ).to eq v
+        end
+      end
+    end
+  end #logged in user context
+end
